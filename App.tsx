e1e72d7f1aaa8b8bcb1e69d1ab1245bb28962c31
ayandip <ayandip.paul@weavers-web.com>
2025-07-18 13:49:58 +0530

    add changes

--- App.tsx
+++ App.tsx
@@ -1,28 +1,22 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from 'react';
+import { Text, View, Button } from 'react-native';
+import './locales/i18n.js'; // import the i18n config
+import { useTranslation } from 'react-i18next';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+export default function App() {
+  const { t, i18n } = useTranslation();
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng);
+  };
 
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+    <View style={{ padding: 40 }}>
+      <Text style={{ fontSize: 24 }}>{t('welcome')}</Text>
+      <Text>{t('greet', { name: 'Ayandip' })}</Text>
+
+      <Button title="Switch to German" onPress={() => changeLanguage('bn')} />
+      <Button title="Switch to English" onPress={() => changeLanguage('en')} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default App;
---
+++ locales/i18n.js
@@ -0,0 +1,22 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+// Import your translations
+import en from '../locales/en/translation.json';
+import bn from '../locales/du/translation.json';
+
+i18n
+    .use(initReactI18next)
+    .init({
+        fallbackLng: 'en',
+        lng: 'en', // default language
+        resources: {
+            en: { translation: en },
+            bn: { translation: bn },
+        },
+        interpolation: {
+            escapeValue: false, // react already protects against XSS
+        },
+    });
+
+export default i18n;
